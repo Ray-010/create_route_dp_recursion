b44abfb14a5977b15973300fc700fe4cb3249b39
Ray-010 <Ray010@outlook.jp>
2023-01-01 18:55:04 +0900

    measure processing time

--- PathCreation.py
+++ PathCreation.py
@@ -20,7 +20,7 @@ class PathCreation:
 
         self.cnt = 0
     
-    def create_map_and_adjacency_list(self):
+    def create_map_and_adjacency_list(self): # 隣接リストの作成
         self.G = [[i for i in range(j*self.width, self.width+j*self.width)] for j in range(self.height)]
         self.adjacency_list = [[] for _ in range(self.height*self.width)]
         for i in range(self.height):
@@ -30,7 +30,7 @@ class PathCreation:
                     if not(0<=i2<self.height and 0<=j2<self.width): continue
                     self.adjacency_list[idx].append(self.G[i2][j2])
     
-    def create_bfs_map(self):
+    def create_bfs_map(self): # BFS到達判定用，マップGの1次元配列
         self.bfs_map = [-1]*(self.element_num)
         self.bfs_map[self.goal_num] = 0
         Q = deque()
@@ -43,8 +43,9 @@ class PathCreation:
                     self.bfs_map[next] = self.bfs_map[now]+1
                     Q.append(next)
 
-    def check_passable(self, route, x) -> bool:
+    def check_passable(self, route, y, x) -> bool: # bfs到達可能判定
         passable = False
+        passable_cnt = 0
         passed_num = set(route[:-1])
         bfs_map = [-1]*(self.element_num)
         bfs_map[self.goal_num] = 0
@@ -55,45 +56,48 @@ class PathCreation:
             for next in self.adjacency_list[now]:
                 if next in passed_num: continue
                 if bfs_map[next] == -1:
+                    passable_cnt += 1
                     bfs_map[next] = bfs_map[now]+1
                     Q.append(next)
-        if bfs_map[x] != -1: passable = True
+        
+        if bfs_map[x] != -1 and (passable_cnt>=self.actual_operation-y): passable = True
         return passable
     
-    def create_dp(self):
+    def create_dp(self): # dpの2次元配列作成(厳密には3次元配列)
         dp = [[[] for _ in range(self.element_num)] for _ in range(self.operation_num)]
         return dp
             
-    def dp_forward(self, dp):
+    def dp_forward(self, dp): # 始点からのdp
         dp[0][self.start_num] = [-1] # 番兵
         for i in self.adjacency_list[self.start_num]:
             dp[1][i].append(self.start_num)
         for i in range(1, self.operation_num-1):
             for j in range(self.element_num):
                 if j == self.start_num: continue
-                # なんか違う気がする
                 if not dp[i][j]: continue # 空
-                
                 for el in self.adjacency_list[j]:
                     if j in dp[i+1][el]: continue
                     if el == self.start_num: continue
                     dp[i+1][el].append(j)
         return dp
     
-    def dp_backward(self, dp1):
+    def dp_backward(self, dp1, ratio): # 終点からのdp
         dp2 = self.create_dp()
-        operation = -1
+        self.actual_operation = -1
+        border = self.element_num*ratio
+        cnt = 0
         for i in range(self.operation_num-1, -1, -1):
             if dp1[i][self.goal_num]:
-                operation = i
-                break
-        if operation == -1: return None
+                actual_operation = i
+                if cnt >= border: break
+            cnt += 1
+        if actual_operation == -1: return None
 
-        dp2[operation][self.goal_num] = [-1] # 番兵
+        dp2[actual_operation][self.goal_num] = [-1] # 番兵
         for i in self.adjacency_list[self.goal_num]:
-            dp2[operation-1][i].append(self.goal_num)
+            dp2[actual_operation-1][i].append(self.goal_num)
 
-        for i in range(operation-1, 0, -1):
+        for i in range(actual_operation-1, 0, -1):
             for j in range(self.element_num):
                 if not dp2[i][j]: continue # 空
                 if j == self.goal_num: continue
@@ -104,7 +108,7 @@ class PathCreation:
                     dp2[i-1][el].append(j)
         return dp2
     
-    def dp_bit(self, dp, memo_size=6): # dpでbit管理
+    def dp_bit(self, dp, memo_size=6): # 作成済みのdpをbitで全探索, 使っていない
         dp3 = [[set() for _ in range(self.element_num)] for _ in range(self.operation_num)]
         dp3[0][self.start_num].add(1)
         for i in range(self.operation_num-1):
@@ -119,7 +123,7 @@ class PathCreation:
                         dp3[i+1][el].add(next)
         return dp3
     
-    def find_route_by_bitdfs(self, dp):
+    def find_route_by_bitmemo_bfs_dfs(self, dp): # bfs到達可能判定とメモ化再帰経路復元
         memo = [set() for _ in range(self.operation_num)]
         route = [self.start_num]
         self.cnt = 0
@@ -130,7 +134,7 @@ class PathCreation:
                 self.cnt += 1
                 route.pop()
                 return bit & ~(1<<x)
-            if not self.check_passable(route, x):
+            if not self.check_passable(route, y, x): # bfs判定
                 self.bfs_cnt += 1
                 memo[x].add(bit)
                 route.pop()
@@ -142,7 +146,6 @@ class PathCreation:
                 next = bit | 1<<i
                 route.append(i)
                 bit = dfs(next, y+1, i)
-            
             if route[-1] == self.goal_num: return bit
             else:
                 memo[x].add(bit)
@@ -150,38 +153,86 @@ class PathCreation:
                 return bit & ~(1<<x)
         
         dfs(1<<self.start_num, 0, self.start_num)
-        print(self.cnt)
-        print(self.bfs_cnt)
+        # print(self.cnt)
+        # print(self.bfs_cnt)
+        return self.cnt, route
+    
+    def find_route_by_bitmemo_dfs(self, dp): # メモ化再帰による経路復元
+        memo = [set() for _ in range(self.operation_num)]
+        route = [self.start_num]
+        self.cnt = 0
+        def dfs(bit, y, x):
+            if route[-1] == self.goal_num: return bit
+            if bit in memo[x]:
+                self.cnt += 1
+                route.pop()
+                return bit & ~(1<<x)
+            passed_num = set(route)
+            for i in dp[y][x]:
+                if i not in set(self.adjacency_list[x]): continue
+                if i in passed_num: continue
+                next = bit | 1<<i
+                route.append(i)
+                bit = dfs(next, y+1, i)
+            if route[-1] == self.goal_num: return bit
+            else:
+                memo[x].add(bit)
+                route.pop()
+                return bit & ~(1<<x)
+        dfs(1<<self.start_num, 0, self.start_num)
+        return self.cnt, route
+    
+    def find_route_by_bfs_dfs(self, dp): # bfs到達可能判定と再帰関数による経路復元
+        def dfs(route, y, x):
+            if route[-1] == self.goal_num: return route
+            if not self.check_passable(route, y, x):
+                route.pop()
+                return route
+            passed_num = set(route)
+            for next in dp[y][x]:
+                if next not in set(self.adjacency_list[x]): continue
+                if next in passed_num: continue
+                route.append(next)
+                route = dfs(route, y+1, next)
+            
+            if route[-1] == self.goal_num: return route
+            else:
+                route.pop()
+                return route
+        
+        route = dfs([self.start_num], 0, self.start_num)
         return route
 
-    def find_route(self, dp):
-        # memo = [set() for _ in range(self.operation_num)]
-        # memo = [[] for _ in range(self.operation_num)]
-        def dfs(route, y, x):
-            # if set(route) in memo[y]:
-            #     self.cnt += 1
-            #     return route[:-1]
-            # if x in memo[y]: return route[:-1]
-                
-            if -1 in dp[y][x]:
-                route.append(-1)
-                return route
-
+    def find_route(self, dp): # 再帰関数による経路復元
+        def dfs(route:list, y, x):
+            if route[-1] == self.goal_num: return route
+            passed_num = set(route)
             for i in dp[y][x]:
                 if route[-1] == -1: return route
-                if i in route: continue
+                if i in passed_num: continue
                 route = dfs(route+[i], y+1, i)
-
-            if route[-1] != -1:
-                # memo[y].add(x)
-                # if set(route[:-1]) not in memo[y]: memo[y].append(set(route[:-1]))
-                return route[:-1]
-            return route
+            if route[-1] == self.goal_num: return route
+            else:
+                route.pop()
+                return route
         route = dfs([self.start_num], 0, self.start_num)
-        print(self.cnt)
-        return route[:-1]
+        return route
+
+    def find_route_ex_dp(self):
+        def dfs(route, x):
+            if len(route) >= self.element_num-1 and route[-1] == self.goal_num: 
+                return route
+            for i in self.adjacency_list[x]:
+                if i not in route:
+                    route = dfs(route+[i], i)
+            if len(route) >= self.element_num-2 and route[-1] == self.goal_num: 
+                return route
+            else: return route[:-1]
+        
+        route = dfs([self.start_num], self.start_num)
+        return route    
     
-    def reformat_map(self, route): # 経路マップ生成
+    def reformat_map(self, route): # routeから経路マップ生成
         numidx = {}
         for i, num in enumerate(route):
             numidx[num] = i
--- main.py
+++ main.py
@@ -1,26 +1,66 @@
 import time
 import sys
+import random
 from PathCreation import PathCreation
 sys.setrecursionlimit(10000000)
 
 def main():
-    width = height = 20
-    sy, sx = 0, 0
-    gy, gx = height-1, width-1
-    pc = PathCreation(width, height, (sy,sx), (gy,gx))
+    # height = random.randint(2, 10)
+    # width = random.randint(2, 10)
+    map_size = [2, 3, 4, 5, 6]
+    fill_ratios = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    map_size = [3]
+    # fill_ratios = [0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
+    fill_ratios = [0]
+    times = 1
 
-    pretime = time.time()
+    for ratio in fill_ratios:
+        print("####################", ratio)
+        fill_ratio = ratio
+        for size in map_size:
+            height = width = size
+            time_record = set()
+            for i in range(times):
+                sy, sx = 0, random.randint(0, width-1)
+                while True:
+                    gy, gx = random.randint(0, height-1), random.randint(0, width-1)
+                    yx = random.choice([0,1])
+                    if yx == 0: gy = random.choice([0, height-1])
+                    else: gx = random.choice([0, width-1])
+                    if not (gx == sx and gy == sy): break
+                pc = PathCreation(width, height, (sy,sx), (gy,gx))
+                
+                pretime = time.time()
+                dp1 = pc.create_dp()
+                dp1 = pc.dp_forward(dp1)
+                cnt = 0
+                bit_cnt = 0
+                while True:
+                    dp2 = pc.dp_backward(dp1, fill_ratio+cnt)
+                    # route = pc.find_route(dp2) # 再帰経路復元
+                    # bit_new_cnt, route = pc.find_route_by_bitmemo_dfs(dp2) # メモ化再帰経路復元
+                    # bit_new_cnt, route = pc.find_route_by_bitmemo_bfs_dfs(dp2) # bfsメモ化再帰経路復元
+                    route = pc.find_route_by_bfs_dfs(dp2) # bfs再帰経路復元
 
-    dp1 = pc.create_dp()
-    dp1 = pc.dp_forward(dp1)
-    dp2 = pc.dp_backward(dp1)
-    route = pc.find_route_by_bitdfs(dp2)
-    newtime = time.time()
-
-    result_route_map = pc.reformat_map(route)
-    for row in result_route_map:
-        print(row)
-    print("実行時間:", newtime-pretime)
+                    # route = pc.find_route_ex_dp() # dp無しの純粋な経路探索
+                    cnt += 0.1
+                    if route: break
+                    if cnt == 1: break
+                result_route_map = pc.reformat_map(route)
+                newtime = time.time()
+                time_record.add(newtime-pretime)
+                # bit_cnt = max(bit_new_cnt, bit_cnt)
+                # print(f"done {i} time")
+            for row in result_route_map:
+                print(row)
+            # for row in dp1:
+            #     print(row)
+            
+            print("size:", size, "ratio:", fill_ratio)
+            # print("bit cnt:", bit_cnt)
+            print("実行平均時間:", sum(time_record)/len(time_record))
+            print("最悪実行時間:", max(time_record))
+            print("最小実行時間:", min(time_record))
 
 if __name__ == "__main__":
     main()
